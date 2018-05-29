15ffdaf7a8a51a3da2313f487226a2b1b11a5975
roman.tarasov <roman.tarasov@benchplatform.com>
2018-05-29 13:38:12 +1000

    service primitive

---
+++ service/run.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type MainFunc func(stopping <-chan struct{}) int
+
+type gracefulShutdown struct {
+	WaitForShutdown  <-chan struct{}
+	ShutdownComplete chan<- int
+	ReturnCode       <-chan int
+}
+
+func runGracefully(timeout time.Duration) *gracefulShutdown {
+	shutdown := make(chan struct{}, 10)
+	shutdownComplete := make(chan int, 10)
+	returnCode := make(chan int, 10)
+
+	gracefulStop := make(chan os.Signal, 10)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+
+	go func() {
+		defer close(shutdown)
+		defer close(gracefulStop)
+		exitCode := 0
+		select {
+		case exitCode = <-shutdownComplete:
+			{
+				log.Printf("Business logic completed. Exit code: %+v", exitCode)
+				returnCode <- exitCode
+				return
+			}
+
+		case sig := <-gracefulStop:
+			log.Printf("Caught sig: %+v", sig)
+			shutdown <- struct{}{}
+		}
+
+		select {
+		case <-time.After(timeout):
+			log.Print("Shutdown timeout occured. Terminating.")
+			returnCode <- 1
+		case exitCode := <-shutdownComplete:
+			log.Print("Shutdown complete.")
+			returnCode <- exitCode
+		}
+	}()
+
+	return &gracefulShutdown{
+		WaitForShutdown:  shutdown,
+		ShutdownComplete: shutdownComplete,
+		ReturnCode:       returnCode,
+	}
+}
+
+func Run(timeout time.Duration, appLogic MainFunc) {
+	graceful := runGracefully(timeout)
+	go func() {
+		graceful.ShutdownComplete <- appLogic(graceful.WaitForShutdown)
+	}()
+	exitCode := <-graceful.ReturnCode
+	log.Printf("Exiting application. Code: %+v", exitCode)
+	os.Exit(exitCode)
+}
